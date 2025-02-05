7b726968957ebcc9160a4651b284c417f9e725f7
Kazi Emran <kaziemran425@gmail.com>
2025-02-05 18:59:30 +0600

    ecommarce

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -13,45 +13,8 @@
         <q-btn flat no-caps label="Login" @click="inception = true" />
         <q-btn label="POST FREE ADD" @click="inception = true" />
       </q-toolbar>
-      <div class="flex flex-center q-py-md">
-        <q-btn
-          flat
-          outline
-          no-caps
-          color="primary"
-          icon="location_on"
-          label="All of Bangladesh"
-          @click="basic = true"
-        />
-      </div>
-
-      <div>
-        <div class="flex flex-center">
-          <div>
-            <q-input
-              outlined
-              rounded
-              bg-color="red"
-              v-model="search"
-              placeholder=" what are you looking for?"
-              hint="Debouncing 500ms"
-            >
-              <template v-slot:append>
-                <q-icon name="search" />
-              </template>
-            </q-input>
-          </div>
-        </div>
-      </div>
     </q-header>
 
-    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
-      <q-list>
-        <q-item-label header> kazi emran </q-item-label>
-
-        <EssentialLink v-for="link in linksList" :key="link.title" v-bind="link" />
-      </q-list>
-    </q-drawer>
     <q-dialog v-model="inception">
       <q-card>
         <q-card-section class="q-pt-none">
@@ -119,7 +82,7 @@
         </q-card-actions>
       </q-card>
     </q-dialog>
-
+    <!--
     <div>
       <div class="q-pa-md q-gutter-sm">
         <q-btn label="Basic scroll" color="primary" @click="basic = true" />
@@ -136,12 +99,12 @@
               <div>Clties</div>
               <div><a href="Dhaka">Dhaka</a></div>
               <div><a href="Chattogram">Chattogram</a></div>
-              <div><a href="Sylhet"></a>Sylhet</div>
-              <div><a href="Kulna"></a>Kulna</div>
-              <div><a href="Barishal"></a>Barishal</div>
-              <div><a href="Commila"></a> Commila</div>
-              <div><a href="Rongpur"></a>Rongpur</div>
-              <div><a href="Rajshahi"></a>Rajshahi</div>
+              <div><a href="Sylhet">Sylhet</a></div>
+              <div><a href="Kulna">Kulna</a></div>
+              <div><a href="Barishal">Barishal</a></div>
+              <div><a href="Commila">Commila</a></div>
+              <div><a href="Rongpur">Rongpur</a></div>
+              <div><a href="Rajshahi">Rajshahi</a></div>
             </q-card-section>
 
             <q-card-actions align="right">
@@ -151,7 +114,13 @@
           </q-card>
         </q-dialog>
       </div>
-    </div>
+    </div> -->
+
+    <q-footer elevated>
+      <q-toolbar>
+        <q-toolbar-title>Footer</q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
 
     <q-page-container>
       <router-view />
@@ -161,73 +130,21 @@
 
 <script>
 import { defineComponent, ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
-
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev',
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework',
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev',
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev',
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev',
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev',
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev',
-  },
-]
 
 export default defineComponent({
   name: 'MainLayout',
 
-  components: {
-    EssentialLink,
-  },
-
   setup() {
     const leftDrawerOpen = ref(false)
     const inception = ref(false)
-    const basic = ref(false)
+    const fixed = ref(false)
 
     return {
-      linksList,
-      leftDrawerOpen,
       toggleLeftDrawer() {
         leftDrawerOpen.value = !leftDrawerOpen.value
       },
       inception,
-      basic,
+      fixed,
     }
   },
 })
---
+++ src/pages/ForntPage.vue
@@ -0,0 +1,438 @@
+<template>
+  <div>
+    <div class="bg-cyan q-pa-md">
+      <div class="q-pa-md q-gutter-sm flex flex-center">
+        <q-btn flat outline icon="location_on" label="All Bangladesh" @click="fixed = true" />
+      </div>
+      <div class="row flex-center">
+        <div>
+          <q-input v-model="search" outlined dense placeholder="Search" style="width: 400px">
+            <template v-slot:append>
+              <q-icon color="amber-13" name="search" />
+            </template>
+          </q-input>
+        </div>
+      </div>
+    </div>
+    <div>
+      <div class="text-bold q-pa-md">
+        <div class="q-py-md">Browse items by category</div>
+      </div>
+      <div>
+        <div class="row">
+          <div class="col">
+            <div>
+              <q-item clickable v-ripple>
+                <q-item-section avatar>
+                  <q-avatar>
+                    <q-img sizes="50px" src="~assets/category_img/mobile.png" />
+                  </q-avatar>
+                </q-item-section>
+                <q-item-section>
+                  <div>Mobiles</div>
+                  <div>1234</div>
+                </q-item-section>
+              </q-item>
+            </div>
+            <div class="q-py-md">
+              <q-item clickable v-ripple>
+                <q-item-section avatar>
+                  <q-avatar>
+                    <q-img sizes="50px" src="~assets/category_img/home.png" />
+                  </q-avatar>
+                </q-item-section>
+                <q-item-section>
+                  <div>Home and Living</div>
+                  <div>1234</div>
+                </q-item-section>
+              </q-item>
+            </div>
+            <div>
+              <div class="q-py-md">
+                <q-item clickable v-ripple>
+                  <q-item-section avatar>
+                    <q-avatar>
+                      <q-img sizes="50px" src="~assets/category_img/sport-ball-football-png.png" />
+                    </q-avatar>
+                  </q-item-section>
+                  <q-item-section>
+                    <div>Hobbies and sports and kids</div>
+                    <div>1234</div>
+                  </q-item-section>
+                </q-item>
+              </div>
+              <div>
+                <div class="q-py-md">
+                  <q-item clickable v-ripple>
+                    <q-item-section avatar>
+                      <q-avatar>
+                        <q-img sizes="50px" src="~assets/category_img/office-bag.png" />
+                      </q-avatar>
+                    </q-item-section>
+                    <q-item-section>
+                      <div>Jobes</div>
+                      <div>1234</div>
+                    </q-item-section>
+                  </q-item>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div>
+              <div class="q-py-md">
+                <q-item clickable v-ripple>
+                  <q-item-section avatar>
+                    <q-avatar>
+                      <q-img sizes="50px" src="~assets/category_img/computer.png" />
+                    </q-avatar>
+                  </q-item-section>
+                  <q-item-section>
+                    <div>Electronics</div>
+                    <div>1234</div>
+                  </q-item-section>
+                </q-item>
+              </div>
+              <div>
+                <div class="q-py-md">
+                  <q-item clickable v-ripple>
+                    <q-item-section avatar>
+                      <q-avatar>
+                        <q-img sizes="50px" src="~assets/category_img/lion-.png" />
+                      </q-avatar>
+                    </q-item-section>
+                    <q-item-section>
+                      <div>Pets & Animals</div>
+                      <div>1234</div>
+                    </q-item-section>
+                  </q-item>
+                </div>
+                <div>
+                  <div class="q-py-md">
+                    <q-item clickable v-ripple>
+                      <q-item-section avatar>
+                        <q-avatar>
+                          <q-img sizes="50px" src="~assets/category_img/house.png" />
+                        </q-avatar>
+                      </q-item-section>
+                      <q-item-section>
+                        <div>Business & Industry</div>
+                        <div>1234</div>
+                      </q-item-section>
+                    </q-item>
+                  </div>
+                  <div>
+                    <div class="q-py-md">
+                      <q-item clickable v-ripple>
+                        <q-item-section avatar>
+                          <q-avatar>
+                            <q-img sizes="50px" src="~assets/category_img/keys-tools-.png" />
+                          </q-avatar>
+                        </q-item-section>
+                        <q-item-section>
+                          <div>Service</div>
+                          <div>1234</div>
+                        </q-item-section>
+                      </q-item>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div>
+              <div class="q-py-md">
+                <q-item clickable v-ripple>
+                  <q-item-section avatar>
+                    <q-avatar>
+                      <q-img sizes="50px" src="~assets/category_img/vehicals.png" />
+                    </q-avatar>
+                  </q-item-section>
+                  <q-item-section>
+                    <div>Vehicles</div>
+                    <div>1234</div>
+                  </q-item-section>
+                </q-item>
+              </div>
+            </div>
+            <div>
+              <div class="q-py-md">
+                <q-item clickable v-ripple>
+                  <q-item-section avatar>
+                    <q-avatar>
+                      <q-img sizes="50px" src="~assets/category_img/t-shirt-.png" />
+                    </q-avatar>
+                  </q-item-section>
+                  <q-item-section>
+                    <div>Mens Fashion & Grooming</div>
+                    <div>1234</div>
+                  </q-item-section>
+                </q-item>
+              </div>
+            </div>
+            <div>
+              <div class="q-py-md">
+                <q-item clickable v-ripple>
+                  <q-item-section avatar>
+                    <q-avatar>
+                      <q-img sizes="50px" src="~assets/category_img/e alphabet.png" />
+                    </q-avatar>
+                  </q-item-section>
+                  <q-item-section>
+                    <div>Essentials</div>
+                    <div>1234</div>
+                  </q-item-section>
+                </q-item>
+              </div>
+            </div>
+            <div>
+              <div class="q-py-md">
+                <q-item clickable v-ripple>
+                  <q-item-section avatar>
+                    <q-avatar>
+                      <q-img sizes="50px" src="~assets/category_img/apple-tree.png" />
+                    </q-avatar>
+                  </q-item-section>
+                  <q-item-section>
+                    <div>Agriculture</div>
+                    <div>1234</div>
+                  </q-item-section>
+                </q-item>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div>
+              <div class="q-py-md">
+                <q-item clickable v-ripple>
+                  <q-item-section avatar>
+                    <q-avatar>
+                      <q-img sizes="50px" src="~assets/category_img/home.png" />
+                    </q-avatar>
+                  </q-item-section>
+                  <q-item-section>
+                    <div>Property</div>
+                    <div>1234</div>
+                  </q-item-section>
+                </q-item>
+              </div>
+            </div>
+            <div>
+              <div class="q-py-md">
+                <q-item clickable v-ripple>
+                  <q-item-section avatar>
+                    <q-avatar>
+                      <q-img sizes="50px" src="~assets/category_img/t-shirt-.png" />
+                    </q-avatar>
+                  </q-item-section>
+                  <q-item-section>
+                    <div>womans Fashion & Beauty</div>
+                    <div>1234</div>
+                  </q-item-section>
+                </q-item>
+              </div>
+            </div>
+            <div>
+              <div class="q-py-md">
+                <q-item clickable v-ripple>
+                  <q-item-section avatar>
+                    <q-avatar>
+                      <q-img
+                        sizes="50px"
+                        src="~assets/category_img/illustration-flat-cartoon-graduation.png"
+                      />
+                    </q-avatar>
+                  </q-item-section>
+                  <q-item-section>
+                    <div>Education</div>
+                    <div>1234</div>
+                  </q-item-section>
+                </q-item>
+              </div>
+            </div>
+            <div>
+              <div class="q-py-md">
+                <q-item clickable v-ripple>
+                  <q-item-section avatar>
+                    <q-avatar>
+                      <q-img sizes="50px" src="~assets/category_img/office-bag.png" />
+                    </q-avatar>
+                  </q-item-section>
+                  <q-item-section>
+                    <div>Overseas Jobes</div>
+                    <div>1234</div>
+                  </q-item-section>
+                </q-item>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div>
+    <div class="row">
+      <div class="col">
+        <div class="q-pa-md">
+          <q-card class="my-card">
+            <div class="q-pa-md">
+              <div class="row">
+                <div class="col">
+                  <div>
+                    <q-img
+                      src="~assets/category_img/large-money-bag.png"
+                      style="width: 250px; height: 250px"
+                    />
+                  </div>
+                </div>
+                <div class="col">
+                  <div class="text-bold text-h5 q-pa-md">Start making money!</div>
+                  <div class="q-pa-md text-weight-thin">
+                    Do you something to sell? <br />post your first ad and start making money
+                  </div>
+                  <div class="q-pa-md">
+                    <q-btn
+                      color="yellow-13"
+                      icon="check"
+                      label="post youir ad for free"
+                      @click="onClick"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </q-card>
+        </div>
+      </div>
+      <div class="col">
+        <div class="q-pa-md">
+          <q-card class="my-card">
+            <div class="q-pa-lg">
+              <q-img
+                src="~src/assets/category_img/Bikroy.com_Logo.png"
+                style="width: 150px; height:"
+              />
+            </div>
+            <div class="text-weight-thin q-pa-md">
+              Looking to hire or get hired in Bangladesh ? <br />Get access to 800k+ CV or broswer
+              through 800+ jobs Vacancies?
+            </div>
+            <div class="q-py-lg">
+              <div class="q-pa-lg">
+                <q-btn
+                  color="indigo-6"
+                  icon-right="chevron_right"
+                  label="Explore More"
+                  @click="onClick"
+                />
+              </div>
+            </div>
+          </q-card>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div>
+    <div class="q-pa-md" style="max-width: 350px">
+      <q-list bordered class="rounded-borders">
+        <q-expansion-item
+          expand-separator
+          icon="perm_identity"
+          label="Account settings"
+          caption="John Doe"
+        >
+          <q-card>
+            <q-card-section>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit
+              eos corrupti commodi magni quaerat ex numquam, dolorum officiis modi facere maiores
+              architecto suscipit iste eveniet doloribus ullam aliquid.
+            </q-card-section>
+          </q-card>
+        </q-expansion-item>
+
+        <q-expansion-item expand-separator icon="signal_wifi_off" label="Wifi settings">
+          <q-card>
+            <q-card-section>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit
+              eos corrupti commodi magni quaerat ex numquam, dolorum officiis modi facere maiores
+              architecto suscipit iste eveniet doloribus ullam aliquid.
+            </q-card-section>
+          </q-card>
+        </q-expansion-item>
+
+        <q-expansion-item expand-separator icon="drafts" label="Drafts" header-class="text-purple">
+          <q-card>
+            <q-card-section>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit
+              eos corrupti commodi magni quaerat ex numquam, dolorum officiis modi facere maiores
+              architecto suscipit iste eveniet doloribus ullam aliquid.
+            </q-card-section>
+          </q-card>
+        </q-expansion-item>
+
+        <q-expansion-item icon="assessment" label="Disabled" disable>
+          <q-card>
+            <q-card-section>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit
+              eos corrupti commodi magni quaerat ex numquam, dolorum officiis modi facere maiores
+              architecto suscipit iste eveniet doloribus ullam aliquid.
+            </q-card-section>
+          </q-card>
+        </q-expansion-item>
+      </q-list>
+    </div>
+  </div>
+  <q-dialog v-model="basic" transition-show="rotate" transition-hide="rotate"> </q-dialog>
+
+  <q-dialog v-model="fixed">
+    <q-card>
+      <q-card-section>
+        <div class="text-h6">Select City or Divition</div>
+      </q-card-section>
+      <q-separator />
+      <q-card-section>
+        <a href="">Bangladesh</a>
+      </q-card-section>
+      <q-separator />
+      <div class="q-pa-md">
+        <div><a href="">Dhaka</a></div>
+        <div><a href="">Barishal</a></div>
+        <div><a href="">comilla</a></div>
+        <div><a href="">Chattogram</a></div>
+        <div><a href="">Rajshahi</a></div>
+        <div><a href="">Rongpur</a></div>
+        <div><a href="">Kulna</a></div>
+        <div><a href="">Mymensing</a></div>
+      </div>
+      <q-card-actions align="right">
+        <q-btn flat label="Decline" color="primary" v-close-popup />
+        <q-btn flat label="Accept" color="primary" v-close-popup />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
+</template>
+<script>
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'ForntPage',
+
+  setup() {
+    const leftDrawerOpen = ref(false)
+    const inception = ref(false)
+    const fixed = ref(false)
+    const search = ref(null)
+
+    return {
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      },
+      inception,
+      fixed,
+      search,
+    }
+  },
+})
+</script>
